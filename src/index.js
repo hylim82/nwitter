a4bb2bb753c01e23e50a1bf85e15c48d4fc384a0
hylim <alreadydiff@gmail.com>
2023-08-19 07:20:16 +0900

    firebase API키 에러가 발생해서, 임시로 수정
    문제점1 : initFirebase -> initializeApp(firebaseConfig)
    -> 단순히 initializeApp()만 호출하면 .env의 값을 제대로 읽어오지 못하는것 같음

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { initFirebase } from 'fbase';
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+//import { getDatabase } from 'firebase/database';
 
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAkxXPG-9I6G6GgYjYRQeN2tCH1uobW7wc",
+  authDomain: "nwitter-d605a.firebaseapp.com",
+  projectId: "nwitter-d605a",
+  storageBucket: "nwitter-d605a.appspot.com",
+  messagingSenderId: "499639799144",
+  appId: "1:499639799144:web:99fcbdf754914efd085add",
+  measurementId: "G-2KQ7F01X8L"
+};
 // Firebase 앱 초기화
-initFirebase();
+const initFb = initializeApp(firebaseConfig);
+console.log(initFb);
+
+const auth = getAuth(initFb); // initFb는 initFirebase()로 초기화한 Firebase 앱 객체
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
