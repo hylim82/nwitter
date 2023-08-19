077d3851e0b9ba263c8e1efacfb8dbd7e81c7076
hylim <alreadydiff@gmail.com>
2023-08-19 13:46:15 +0900

    회원 가입 버튼 생성

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    //console.log(event.target.name); // 입력이 제대로 동작되는지 확인
+    const {target:{name, value}} = event; // TODO : 이게 이해가 잘안됨
+    // console.log(value); // 입력이 제대로 동작되는지 확인
+    if(name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  }
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div>
-      Auth
+      Auth Page
+      <form onSubmit={onSubmit}>
+        <input 
+          name="email" 
+          type="email" 
+          placeholder='Email' required
+          value={email}
+          onChange={onChange} // 변경될 때마다, onChange event발생
+          />
+        <input 
+          name="password" 
+          type="password" 
+          placeholder='Password' required
+          value={password}
+          onChange={onChange} // 변경될 때마다, onChange event발생
+          />
+        <input type="submit" value="Log in"/>
+      </form>
+      <div>
+        <button>Continute with google</button>
+        <button>Continute with google</button>
+      </div>
     </div>
   );
 };
