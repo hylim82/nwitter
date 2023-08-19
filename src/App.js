5778c60ee2fc6f419ea2ffa62aff90d6e8e88835
hylim <alreadydiff@gmail.com>
2023-08-19 17:35:44 +0900

    [Firebase] 수정 사항
    Auth.js
    1) auth -> authService로 수정
    2) errorHandle함수 추가
    - error.code를 받아와서 setError로 에러출력
    3) toggleAccount 변수로 newAccount와 반대되는 상태를 가져옴
    -> setNewAccount로 prev를 주면 !prev를 반환하도록함
    
    App.js
    1) init state를 추가해서 AppRouter 컴포넌트 or "Initializing" 을 보여줄지 선택
    2) useEffect를 사용해서 isLoggedIn과 init state 설정
    TODO :

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AppRouter from "./components/Router";
 import authService from 'index';
 
 function App() {
   // console.log(authService.currentUser);
-  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(authService.currentUser);
+  //TODO : useEffect는 뭐지??
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if(user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setInit(true);
+    });
+  }, []);
   return (
     <div>
-      <AppRouter isLoggedIn={isLoggedIn} />
-      <footer>&copy; Nwitter {new Date().getFullYear()}</footer>
+      {init ?<AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter </footer>
     </div>
   );
 }
--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -5,7 +5,8 @@ const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [newAccount, setNewAccount] = useState(true);
-  const auth = getAuth(); // Firebase 인증 객체 가져오기
+  const [error, setError] = useState("");
+  const authService = getAuth(); // Firebase 인증 객체 가져오기
 
   const onChange = (event) => {
     const { target: { name, value } } = event;
@@ -16,22 +17,38 @@ const Auth = () => {
     }
   };
 
+  function errorHandle(errorCode) {
+    if(errorCode === 'auth/email-already-in-use') {
+      setError("if there already exists an account with the given email address.");
+    } else if(errorCode === 'auth/invalid-email') {
+      setError("the email address is not valid.");
+    } else if(errorCode === 'auth/operation-not-allowed') {
+      setError("email/password accounts are not enabled. Enable email/password accounts in the Firebase Console, under the Auth tab.");
+    } else if (errorCode === 'auth/weak-password') {
+      setError('the password is not strong enough.');
+    }
+  }
+
   const onSubmit = async (event) => {
     event.preventDefault();
     try {
       let data;
       if (newAccount) {
         // create account
-        data = await createUserWithEmailAndPassword(auth, email, password);
+        data = await createUserWithEmailAndPassword(authService, email, password);
       } else {
         // Log in
-        data = await signInWithEmailAndPassword(auth, email, password);
+        data = await signInWithEmailAndPassword(authService, email, password);
       }
       console.log(data);
     } catch (error) {
-      console.log(error);
+      // console.log(error);
+      errorHandle(error.code);
+        
     }
   };
+
+  const toggleAccout = () => setNewAccount((prev => !prev));
 
   return (
     <div>
@@ -51,8 +68,10 @@ const Auth = () => {
           value={password}
           onChange={onChange}
         />
-        <input type="submit" value={newAccount === true ? "Create Account" : "Log in"} />
+        <input type="submit" value={newAccount  ? "Create Account" : "Log in"} />
+        {error}
       </form>
+      <span onClick={toggleAccout}>{newAccount ? "Sign In." :"Create Accout"}</span>
       <div>
         <button>Continue with Google</button>
         <button>Continue with Apple</button>
