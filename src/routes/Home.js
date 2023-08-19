bbfbe317e37750ce73c1531e5cb88f95c6b8f1e3
hylim <alreadydiff@gmail.com>
2023-08-20 00:32:57 +0900

    [firestore] firestore 데이터 저장 기능 확인

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,11 +1,24 @@
 import React, { useState } from 'react';
+import { dbService } from 'fbase';
+import { collection, addDoc } from 'firebase/firestore'; // 필요한 모듈만 가져오기
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
-  }
+    try {
+      // nweets 컬렉션에 새 문서 추가
+      await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now()
+      });
+
+      setNweet("");
+    } catch (error) {
+      console.error("Error adding nweet: ", error);
+    }
+  };
 
   const onChange = (event) => {
     const {target:{value}} = event;
