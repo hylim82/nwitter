ea323f254a8a6d7d4bd2c4863e8347c8998564cd
hylim <alreadydiff@gmail.com>
2023-08-21 00:56:10 +0900

    firebase hosting 구현 (github hosting을 위한 gt-pages모듈 설치)
    TODO : social login이 데스크탑에서만 되는 문제 수정 필요
    
    # npm run deploy
    # firebase deploy --only hostingProject Console:
    
    https://console.firebase.google.com/project/nwitter-d605a/overview
    Hosting URL: https://nwitter-d605a.web.app

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
